0abd9761369f97000b8cc462792213c8b6c1550a
javierdlrc <javierdlrc@JAVIERDELOS6BBB>
2024-02-29 10:32:53 -0700

    Controller update 1

--- Mission08_Team0309/Controllers/HomeController.cs
+++ Mission08_Team0309/Controllers/HomeController.cs
@@ -5,11 +5,42 @@ using System.Diagnostics;
 namespace Mission08_Team0309.Controllers
 {
     public class HomeController : Controller
-    {    
+    {
+        private ToDoListContext _context;
+
+        public HomeController(ToDoListContext temp)
+        {
+            _context = temp;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Task()
+        {
+            ViewBag.Category = _context.Category
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+            return View("Task");
+        }
+
+        [HttpPost]
+        public IActionResult Task(Item response)
+        {
+            _context.Item.Add(response);
+            _context.SaveChanges();
+
+            return View( response);
+        }
+
+        public IActionResult Quadrant()
+        {
+            var item = _context.Item
+                .OrderBy(x => x.DueDate).ToList();
+
+            return View(item);
+        }
     }
 }
