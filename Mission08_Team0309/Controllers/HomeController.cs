3daecaece08120d7edab0fcdbae4023cc47d7b7c
javierdlrc <javierdlrc@JAVIERDELOS6BBB>
2024-03-01 14:06:17 -0700

    controller update 4

--- Mission08_Team0309/Controllers/HomeController.cs
+++ Mission08_Team0309/Controllers/HomeController.cs
@@ -24,19 +24,33 @@ namespace Mission08_Team0309.Controllers
         [HttpGet]
         public IActionResult Tasks()
         {
+            
             ViewBag.Category = _context.Categories
                 .OrderBy(x => x.CategoryName)
                 .ToList();
-            return View("Tasks");
+            return View("Tasks", new Item());
         }
 
         [HttpPost]
         public IActionResult Tasks(Item response)
         {
-            _context.Items.Add(response);
-            _context.SaveChanges();
+            if (ModelState.IsValid)
+            {
+                _context.Items.Add(response);
+                _context.SaveChanges();
 
-            return View(response);
+                return View(response);
+            }
+
+            else
+            {
+                ViewBag.Category = _context.Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+                return View(response);
+            }
+            
+            
         }
 
         public IActionResult Quadrant()
@@ -50,12 +64,9 @@ namespace Mission08_Team0309.Controllers
         [HttpGet]
         public IActionResult Edit(int id)
         {
-            var recordToEdit = _context.Items
-                .Where(x => x.Id == id);
+            var recordToEdit = _context.Items.FirstOrDefault(x => x.Id == id);
 
-            ViewBag.Categories = _context.Categories
-               .OrderBy(x => x.CategoryName)
-               .ToList();
+            ViewBag.Categories = _context.Categories.OrderBy(x => x.CategoryName).ToList();
 
             return View("Tasks", recordToEdit);
         }
@@ -72,8 +83,7 @@ namespace Mission08_Team0309.Controllers
         [HttpGet]
         public IActionResult Delete(int id) 
         {
-            var recordToDelete = _context.Items
-                .Single(x => x.Id == id);
+            var recordToDelete = _context.Items.FirstOrDefault(x => x.Id == id);
 
             return View(recordToDelete);
         }
